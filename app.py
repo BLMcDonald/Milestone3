441b6df4d7045efa9d8cc1203e0f5fd9e0f57c15
Ubuntu <ubuntu@ip-172-31-23-54.us-east-2.compute.internal>
2019-09-05 19:48:53 +0000

    connect to database

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = 'dictionary'
+
+mongo = PyMongo(app)
+
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_words')
+def get_words():
+    return render_template("words.html", words=mongo.db.words.find())
 
 
 if __name__ == '__main__':
